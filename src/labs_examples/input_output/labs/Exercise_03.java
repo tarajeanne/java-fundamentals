57775872ef1ef1b7a4d39a960195d1c4c82a48e6
Tara Patel <t.jeanne.patel@gmail.com>
2021-03-09 15:04:56 -0600

    finished exercise 3 of input/output

--- src/labs_examples/input_output/labs/Exercise_03.java
+++ src/labs_examples/input_output/labs/Exercise_03.java
@@ -1,5 +1,11 @@
 package labs_examples.input_output.labs;
 
+import javax.sound.midi.Sequence;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.SequenceInputStream;
+
 /**
  * Input/Output Exercise 3: variety
  *
@@ -9,3 +15,28 @@ package labs_examples.input_output.labs;
  *    4) Demonstrate the use of the DataInputStream and DataOutputStream
  *
  */
+
+public class Exercise_03 {
+    public static void main(String[] args) {
+        String inputPath1 = "src/labs_examples/input_output/files/byte_data";
+        String inputPath2 = "src/labs_examples/input_output/files/byte_data2";
+        FileInputStream file1 = null;
+        FileInputStream file2 = null;
+        SequenceInputStream sequence = null;
+        try {
+            file1 = new FileInputStream(inputPath1);
+            file2 = new FileInputStream(inputPath2);
+            sequence = new SequenceInputStream(file1, file2);
+            int i;
+            while ((i = sequence.read()) != -1) {
+                System.out.print((char)i);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
