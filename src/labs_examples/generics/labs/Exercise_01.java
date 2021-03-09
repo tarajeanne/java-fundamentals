6ce6fedd77a82990b031cd990826dec9860fa477
Tara Patel <t.jeanne.patel@gmail.com>
2021-03-09 15:24:39 -0600

    finished generics exercise 1

--- src/labs_examples/generics/labs/Exercise_01.java
+++ src/labs_examples/generics/labs/Exercise_01.java
@@ -8,3 +8,35 @@ package labs_examples.generics.labs;
  *          dynamic usage.
  */
 
+public class Exercise_01<E> {
+    private E first;
+    private E second;
+    public Exercise_01(E first, E second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public E getFirst() {
+        return first;
+    }
+
+    public void setFirst(E first) {
+        this.first = first;
+    }
+
+    public E getSecond() {
+        return second;
+    }
+
+    public void setSecond(E second) {
+        this.second = second;
+    }
+
+    public static void main(String[] args) {
+        Exercise_01<Integer> obj = new Exercise_01<>(5, 6);
+        System.out.println(obj.getFirst());
+
+        Exercise_01<Double> obj2 = new Exercise_01<>(7.0, 5.5);
+        System.out.println(obj2.getSecond());
+    }
+}
