84ec9ddf50fc8ada11b92300f0560c77648ddf57
Tara Patel <t.jeanne.patel@gmail.com>
2021-03-20 09:46:24 -0500

    finished Generics exercises

--- src/labs_examples/generics/labs/Exercise_03.java
+++ src/labs_examples/generics/labs/Exercise_03.java
@@ -2,6 +2,7 @@ package labs_examples.generics.labs;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * Generics Exercise 3:
@@ -17,3 +18,72 @@ import java.util.Collection;
  *      4) Write a generic method to find the largest element within the range (begin, end) of a list.
  *
  */
+public class Exercise_03 {
+    public <C extends Collection<String>> int count(C list) {
+        int count = 0;
+        for (String i : list) {
+            count ++;
+        }
+        return count;
+    }
+
+    public <E> void swap(E[] arr, int i, int j) {
+        E temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public <E extends Comparable<E>, L extends List<E>> E largest(L list, int beg, int end) {
+        E max = list.get(beg);
+        for (int i = beg + 1; i < end; i++) {
+            E curr = list.get(i);
+            int comparison = curr.compareTo(max);
+            if (comparison > 0) {
+                max = curr;
+            }
+        }
+        return max;
+    }
+
+    public <C extends Collection<String>> int countPalindromes(C list) {
+        int count = 0;
+        for (String word : list) {
+            boolean isPalindrome = true;
+            for (int i = 0; i <= word.length() / 2; i++) {
+                char letter1 = word.charAt(i);
+                int index2 = word.length() - i - 1;
+                char letter2 = word.charAt(index2);
+                if (letter1 != letter2) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+            if (isPalindrome) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Exercise_03 obj = new Exercise_03();
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("It");
+        list.add("loveevol");
+        list.add("chicken");
+        list.add("racecar");
+        System.out.println(obj.count(list));
+
+        String[] list2 = {"It", "love", "chicken", "racecar"};
+        Exercise_03 obj2 = new Exercise_03();
+        System.out.println(obj2.count(list));
+
+        obj.swap(list2, 1, 3);
+        for (String i : list2) {
+            System.out.println(i);
+        }
+
+        System.out.println(obj.countPalindromes(list));
+
+    }
+}
